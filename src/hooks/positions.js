51eae939692402bf325ba0ac73a6e624afb50373
JimZer <jimi.vaubien@protonmail.com>
2020-05-03 14:25:09 +0200

    fixe loading in positions pagination change

--- src/hooks/positions.js
+++ src/hooks/positions.js
@@ -1,18 +1,21 @@
-import { useState, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { POSITIONS } from 'gql/queries';
 
 export default (avamCode) => {
   const [page, setPage] = useState(1);
+  const [positions, setPositions] = useState([]);
+  const [totalCount, setTotalCount] = useState(0);
   const { loading, error, data } = useQuery(POSITIONS, {
     variables: { professionCodes: [avamCode], page },
   });
 
-  console.log(error);
-  if (loading || error) return { positions: [], loading };
+  useEffect(() => {
+    if (!loading && !error && data) {
+      setPositions(data.positions.positions);
+      setTotalCount(data.positions.totalCount);
+    }
+  }, [data]);
 
-  const {
-    positions: { positions, totalCount },
-  } = data;
-  return { positions, totalCount, page, setPage };
+  return { positions, totalCount, page, setPage, loading };
 };
